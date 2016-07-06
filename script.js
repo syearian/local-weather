92893c03cb917080321b249a58c33824903d1e96
Steven Yearian <syearian@gmail.com>
2016-07-06 12:12:09 -0500

    Add "&units=imperial" to apiUrl

--- script.js
+++ script.js
@@ -17,63 +17,64 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
-      apiUrl = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=cfc2eaa1c51253a29ce7206e1aad37c9";
+      apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=cfc2eaa1c51253a29ce7206e1aad37c9";
     });
   } // Get client location data
 
-  $.getJSON(apiUrl, function(data) {
-    $("#city").html(data.name);
-    var dateObject = new Date(data.dt);
-    var month = "";
-    switch (dateObject.getMonth()) {
-      case 0:
-      month = "Jan";
-      break;
-      case 1:
-      month = "Feb";
-      break;
-      case 2:
-      month = "March";
-      break;
-      case 3:
-      month = "April";
-      break;
-      case 4:
-      month = "May";
-      break;
-      case 5:
-      month = "June";
-      break;
-      case 6:
-      month = "July";
-      break;
-      case 7:
-      month = "Aug";
-      break;
-      case 8:
-      month = "Sept";
-      break;
-      case 9:
-      month = "Oct";
-      break;
-      case 10:
-      month = "Nov";
-      break;
-      case 11:
-      month = "Dec";
-      break;
-    }
-    var date = month + " " + dateObject.getDate();
-    $("#date").html(date);
-    $("#temp").html(data.main.temp + " &deg;F");
-    $("#humidity").html(data.main.humidity + "&#37;");
-    $("#decription").html(data.weather.description);
-    var iconCode = data.weather.icon;
-    $("#icon").attr("src", + "http://openweathermap.org/img/w/" + iconCode + ".png");
-  }); // get JSON weather data and add it to page
+  getWeather();
 
 });
 
-// function getWeather() {
-  
-// } 
+function getWeather() {
+  $.getJSON(apiUrl, function(json) {
+    var data = json;
+    $("#city").html(data.name);
+    var date = new Date(data.dt);
+    var month = "";
+    switch (date.getMonth()) {
+      case 0:
+        month = "Jan";
+        break;
+      case 1:
+        month = "Feb";
+        break;
+      case 2:
+        month = "March";
+        break;
+      case 3:
+        month = "April";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "June";
+        break;
+      case 6:
+        month = "July";
+        break;
+      case 7:
+        month = "Aug";
+        break;
+      case 8:
+        month = "Sept";
+        break;
+      case 9:
+        month = "Oct";
+        break;
+      case 10:
+        month = "Nov";
+        break;
+      case 11:
+        month = "Dec";
+        break;
+    }
+    var today = month + " " + date.getDate();
+    $("#date").html(today);
+    $("#temp").html(data.main.temp + " &deg;F");
+    $("#humidity").html(data.main.humidity + "&#37;");
+    $("#description").html(data.weather[0].description);
+    var iconCode = data.weather[0].icon;
+    $("#icon").attr("src", "http://openweathermap.org/img/w/" + iconCode + ".png");
+  }); // get JSON weather data and add it to page
+} 
