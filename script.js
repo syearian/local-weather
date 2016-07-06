f3704e6d140160a53c766fc8037c12f194753f9e
Steven Yearian <syearian@gmail.com>
2016-07-06 13:26:20 -0500

    Move setting of apiUrl into getWeather()

--- script.js
+++ script.js
@@ -17,15 +17,14 @@ $(document).ready(function() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=cfc2eaa1c51253a29ce7206e1aad37c9";
+        lon = position.coords.longitude;        
         });
       }
   } // Get client location data
 
-  function getWeather() {
-    $.getJSON(apiUrl, function(json) {
-      var data = json;
+  function getWeather(lat, lon) {
+    apiUrl = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=cfc2eaa1c51253a29ce7206e1aad37c9";
+    $.getJSON(apiUrl, function(data) {
       $("#city").html(data.name);      
       $("#temp").html(data.main.temp + " &deg;F");
       $("#humidity").html(data.main.humidity + "&#37;");
@@ -36,6 +35,6 @@ $(document).ready(function() {
   } 
   
   getLocation();
-  getWeather();
+  getWeather(lat, lon);
 
 });
