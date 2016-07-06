303e6db7913270d57dbf1432ca098c99ec0acce4
Steven Yearian <syearian@gmail.com>
2016-07-06 15:00:54 -0500

    Add weather-icons

--- script.js
+++ script.js
@@ -32,10 +32,12 @@ $(document).ready(function() {
     apiUrl = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=cfc2eaa1c51253a29ce7206e1aad37c9";
     $.getJSON(apiUrl, function(data) {
       $("#city").html(data.name);      
-      $("#temp").html(data.main.temp + " &deg;F");
-      $("#humidity").html(data.main.humidity + "&#37;");
+      $("#temp").html('<i class="wi wi-thermometer"></i> ' + data.main.temp + ' <i class="wi wi-fahrenheit"></i>');
+      var id = data.weather[0].id;
+      $("#icon").addClass("wi-owm-" + id);
       $("#description").html(data.weather[0].description);
-      var iconCode = data.weather[0].icon;
-      $("#icon").attr("src", "http://openweathermap.org/img/w/" + iconCode + ".png");
+      $("#humidity").html('<i class="wi wi-humidity"></i> ' + data.main.humidity + "&#37;");
+      var windDir = Math.round(data.wind.deg);
+      $("#wind").html('<i class="wi wi-wind from-' + windDir + '-deg"></i> ' + data.wind.speed + " mph");
     }); // get JSON weather data and add it to page
   }
